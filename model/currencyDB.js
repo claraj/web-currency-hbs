7e3f0e1b304f8234ea6175f0859f3ede7004a433
clara <clara@clara.com>
2017-10-07 10:57:28 -0500

    express-handlebars currency conversion site

---
+++ model/currencyDB.js
@@ -0,0 +1,6 @@
+module.exports = {
+  EUR: 0.85,
+  JPY: 112.63
+};
+
+
---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var exchangeRates = require('../model/currencyDB');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title : 'Express' });
+});
+
+/* GET convert page */
+router.get('/convert', function(req, res, next){
+
+  var dollars = req.query.dollars;   // How many dollars?
+  var toCurrency = req.query.to_currency;  // To what currency?
+
+  var converted = dollars * exchangeRates[toCurrency];   // math!
+
+  res.render('results', {
+    dollars: dollars,
+    toCurrency: toCurrency,
+    converted: converted}
+  );
+
+});
+
+/* GET about page */
+router.get('/about', function(req, res, next){
+  res.render('about', { name: "My awesome site"});
+});
+
+
+
+module.exports = router;
+
+
+
+
+
